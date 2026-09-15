01f14143067c9c944e656adf532e2de023bf5ddc
SmartPack Team <smartpack@sih2026.com>
2026-09-15 09:17:09 +0530

    Add GitHub Actions workflow for automatic deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let repo = 'SIH-2026';
+
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: isGithubActions ? `/${repo}` : '',
+  assetPrefix: isGithubActions ? `/${repo}/` : '',
   eslint: {
     ignoreDuringBuilds: true,
   },
